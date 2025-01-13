6cc511b3ab6688fa6daf86106dabaa452075c0e6
Reeyyyh <rhnlnv@gmail.com>
2025-01-13 11:33:19 +0700

    sticky header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
     return (
-        <header className="bg-blue-500 text-white py-4 px-6 shadow-md">
+        <header className="bg-blue-500 text-white py-4 px-6 shadow-md sticky top-0">
             <h1 className="text-2xl font-bold">Country list website</h1>
         </header>
     );
